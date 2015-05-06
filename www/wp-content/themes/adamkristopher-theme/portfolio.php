17fbbef0cd1ad2f9feb48b5dfe6364d5d2ae357a
Adam.Carter <adamkristopher@gmail.com>
2015-05-06 15:08:01 -0500

    Portfolio page updates

--- www/wp-content/themes/adamkristopher-theme/portfolio.php
+++ www/wp-content/themes/adamkristopher-theme/portfolio.php
@@ -2,13 +2,31 @@
 /*
  * Template Name: Portfolio
  */
-get_header() ?>
+get_header();
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
+$args = array(
+	'post_type' => 'portfolio',
+	'posts_per_page' => '50',
+	);
 
-	<h1><?php the_title() ?></h1>
-	<?php the_content() ?>
+$portfolio = new WP_Query( $args ) ?>
+
+<?php if ( $portfolio->have_posts() ) : while ( $portfolio->have_posts() ) : $portfolio->the_post() ?>
+
+	<div class="portfolio">
+		<h1><?php the_title() ?></h1>
+		<p><?php the_field( 'description' ) ?></p>
+
+		<div class="portfolio-image">
+			<a href="<?php the_field( 'project_link' ) ?>" target="_blank"><img src="<?php the_field( 'photo' ) ?>"/></a>
+		</div>
+
+		<p><?php the_field( 'description' ) ?></p>
+	</div>
+
 
 <?php endwhile; endif ?>
+
+<?php wp_reset_postdata() ?>
 
 <?php get_footer() ?>
