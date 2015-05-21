7751ebc22e8de5de8e6ad9e226848b46ec798260
Adam.Carter <adamkristopher@gmail.com>
2015-05-21 15:17:03 -0500

    About Page  styles

--- www/wp-content/themes/adamkristopher-theme/about.php
+++ www/wp-content/themes/adamkristopher-theme/about.php
@@ -6,8 +6,19 @@
 get_header() ?>
 
 <section class="about grid-container">
+	<div class="homepage-content grid-60">
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post() ?>
+		<div class="blog-post grid-100">
+			<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
+			<HR WIDTH="100%" COLOR="#ee6930" SIZE="1">
+			<p><?php the_content() ?></p>
+		</div>
+	<?php endwhile; endif ?>
+	</div>
 
-
+	<div class="sidebar grid-30">
+		<?php get_sidebar( 'homepage' ); ?>
+	</div>
 </section>
 
 <?php get_footer() ?>
