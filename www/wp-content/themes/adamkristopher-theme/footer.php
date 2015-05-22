4e731f23ea81aa4c2568a28a323806692576311c
Adam.Carter <adamkristopher@gmail.com>
2015-05-22 00:08:53 -0500

    social media addresses

--- www/wp-content/themes/adamkristopher-theme/footer.php
+++ www/wp-content/themes/adamkristopher-theme/footer.php
@@ -6,11 +6,11 @@
 			</div>
 			<div class="icons grid-100 tablet-grid-100 mobile-grid-100">
 						<div class="ss-icon">
-							<a href="">&#xF610;</a>
-							<a href="">&#xF611;</a>
-							<a href="">&#xF612;</a>
-							<a href="">&#xF671;</a>
-							<a href="">&#xF613;</a>
+							<a href="https://www.facebook.com/adam.carter.3990" target="_blank">&#xF610;</a>
+							<a href="https://twitter.com/MagnaCarter78" target="_blank">&#xF611;</a>
+							<a href="https://www.linkedin.com/profile/view?id=352754029&trk=nav_responsive_tab_profile_pic" target="_blank">&#xF612;</a>
+							<a href=" https://github.com/Magnacarter" target="_blank">&#xF671;</a>
+							<a href="https://plus.google.com/u/0/102918130283247028095/posts" target="_blank">&#xF613;</a>
 						</div>
 					</div>
 
