c75c9d7c97a9b42ba2e3ab7564d1253ad2f0617a
Adam.Carter <adamkristopher@gmail.com>
2015-06-04 05:26:06 -0500

    Styles

--- www/wp-content/themes/adamkristopher-theme/loop.php
+++ www/wp-content/themes/adamkristopher-theme/loop.php
@@ -1,14 +1,13 @@
 <article>
-
 	<h3><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h3>
 
 	<ul class="info">
-		<li>Posted in: <?php the_category( ', ' ) ?></li>
-		<li>Written by: <a href="<?php bloginfo( 'siteurl' ) ?>/about/"><?php the_author() ?></a></li>
-		<li>On: <?php the_time( 'F j, Y' ); ?></li>
+		<li>By <a href="<?php bloginfo( 'siteurl' ) ?>/about/"><?php the_author() ?></a></li>
+		<li><?php the_time( 'F j, Y' ); ?></li>
+		<li><?php the_category( ', ' ) ?></li>
 	</ul>
 
-	<div class="excerpt">
+	<div class="excerpt grid-65">
 
 		<?php if ( is_single() ) : ?>
 
@@ -26,4 +25,7 @@
 
 	</div>
 
+	<div class="blog-sidebar grid-30">
+		<?php get_sidebar( 'homepage' ) ?>
+	</div>
 </article>
--- www/wp-content/themes/adamkristopher-theme/single.php
+++ www/wp-content/themes/adamkristopher-theme/single.php
@@ -2,15 +2,11 @@
 
 <div id="single-post" class="grid-container">
 
-	<div class="grid-75">
-
 		<?php while ( have_posts() ) : the_post() ?>
 
 			<?php get_template_part( 'loop' ) ?>
 
 		<?php endwhile ?>
-
-	</div>
 
 </div>
 
