460dbb687fb2a7f36bdff0b24c9ad46c106d91dd
Adam.Carter <adamkristopher@gmail.com>
2015-08-12 03:29:36 -0500

    portfolio js

--- www/wp-content/themes/adamkristopher-theme/js/theme.js
+++ www/wp-content/themes/adamkristopher-theme/js/theme.js
@@ -1,4 +1,14 @@
 jQuery( document ).ready( function($) {
+	//Hover elements for single work on portfolio page
+	var singleWork = $( '.portfolio-content' );
 
+	singleWork.hover(
+		function() {
+			$( this ).find( '.feature-hover' ).fadeIn(400);
+		},
+		function() {
+			$( this ).find( '.feature-hover' ).fadeOut(200);
+		}
+	);
 });
 
