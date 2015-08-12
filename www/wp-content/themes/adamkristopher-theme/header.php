212de2d35f98cd4cdd796c7afe027d4e87655593
Adam.Carter <adamkristopher@gmail.com>
2015-08-12 08:08:49 -0500

    remove google verify stuff

--- www/wp-content/themes/adamkristopher-theme/header.php
+++ www/wp-content/themes/adamkristopher-theme/header.php
@@ -21,7 +21,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="google-site-verification" content="3FukpFQJKiIV3Vss5z8rakIcUTE3SlyXqwCkpGxEg34" />
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
